31c601c245b5a36a5e634eb65fec762ba2bd76b2
SQL <i.sql@inbox.ru>
2021-09-15 12:13:08 +0300

    Create Maze.cpp

---
+++ Maze.cpp
@@ -0,0 +1,24 @@
+﻿#include "Tool\include.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	get_info_game();
+
+	while (1) 
+	{
+		string command;
+		double var; // int var;
+
+		cout << ">>> Введите команду: ";
+		cin >> command;
+
+		cout << ">>> Значение: ";
+		cin >> var;
+
+		SetValueFLOAT(command.c_str(), var); // SetValueINT(command.c_str(), var);
+		system("cls");
+	}
+}
